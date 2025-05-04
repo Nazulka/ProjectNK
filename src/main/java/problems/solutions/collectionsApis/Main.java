b83e95b4b1d9bc349197ee9f4e95cd7f1e25e332
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-05-04 11:02:17 +0100

    List interface implementation

--- src/main/java/problems/solutions/collectionsApis/Main.java
+++ src/main/java/problems/solutions/collectionsApis/Main.java
@@ -5,15 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //List, Set, Map
-        // ArrayList - Ordered, resizable array, can contain duplicates
-        // we can append elements to the list, iterate etc
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(100);
-        list.add(0, 200);
-        list.addFirst(450);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
+        Student le = new Student();
+        le.list();
     }
 }
